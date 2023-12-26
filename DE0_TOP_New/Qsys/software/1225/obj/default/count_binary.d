009d5629fd5ca30fe2e800df65fe4e589f3d0315
jqkh <max0970943943@gmail.com>
2023-12-26 13:53:52 +0800

    1

---
+++ DE0_TOP_New/Qsys/software/1225/obj/default/count_binary.d
@@ -0,0 +1,29 @@
+obj/default/count_binary.o: count_binary.c count_binary.h \
+  ../1225_bsp//HAL/inc/alt_types.h \
+  ../1225_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../1225_bsp//HAL/inc/io.h ../1225_bsp//HAL/inc/alt_types.h \
+  ../1225_bsp//HAL/inc/sys/alt_irq.h ../1225_bsp//HAL/inc/nios2.h \
+  ../1225_bsp//HAL/inc/alt_types.h ../1225_bsp/system.h \
+  ../1225_bsp/linker.h ../1225_bsp/system.h
+
+count_binary.h:
+
+../1225_bsp//HAL/inc/alt_types.h:
+
+../1225_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../1225_bsp//HAL/inc/io.h:
+
+../1225_bsp//HAL/inc/alt_types.h:
+
+../1225_bsp//HAL/inc/sys/alt_irq.h:
+
+../1225_bsp//HAL/inc/nios2.h:
+
+../1225_bsp//HAL/inc/alt_types.h:
+
+../1225_bsp/system.h:
+
+../1225_bsp/linker.h:
+
+../1225_bsp/system.h:
