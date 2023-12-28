8750b76e20c73eded1960468c6f54d06b0e331b7
max <411508053@o365.tku.edu.tw>
2023-12-28 16:14:03 +0800

    .

---
+++ DE0_TOP_New/Qsys/software/1229/obj/default/count_binary.d
@@ -0,0 +1,38 @@
+obj/default/count_binary.o: count_binary.c ../1229_bsp/system.h \
+  ../1229_bsp/linker.h ../1229_bsp//HAL/inc/io.h \
+  ../1229_bsp//HAL/inc/alt_types.h ../1229_bsp/star.c ../1229_bsp/pio.h \
+  ../1229_bsp//HAL/inc/sys/alt_irq.h ../1229_bsp//HAL/inc/nios2.h \
+  ../1229_bsp//HAL/inc/alt_types.h \
+  ../1229_bsp//HAL/inc/priv/alt_legacy_irq.h ../1229_bsp//HAL/inc/nios2.h \
+  ../1229_bsp//HAL/inc/alt_types.h ../1229_bsp/pio.c ../1229_bsp/pio.h \
+  ../1229_bsp/hex.c
+
+../1229_bsp/system.h:
+
+../1229_bsp/linker.h:
+
+../1229_bsp//HAL/inc/io.h:
+
+../1229_bsp//HAL/inc/alt_types.h:
+
+../1229_bsp/star.c:
+
+../1229_bsp/pio.h:
+
+../1229_bsp//HAL/inc/sys/alt_irq.h:
+
+../1229_bsp//HAL/inc/nios2.h:
+
+../1229_bsp//HAL/inc/alt_types.h:
+
+../1229_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../1229_bsp//HAL/inc/nios2.h:
+
+../1229_bsp//HAL/inc/alt_types.h:
+
+../1229_bsp/pio.c:
+
+../1229_bsp/pio.h:
+
+../1229_bsp/hex.c:
